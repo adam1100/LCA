9b50e28653435c755ace242850b0a85815d16848
Adam Paul Lysaght <alysaght@tcd.ie>
2020-10-22 20:54:18 +0100

    added some java testing

--- Java/LCA.java
+++ Java/LCA.java
@@ -18,7 +18,6 @@ public class LCA {
     private List<Integer> path1 = new ArrayList<>(); 
     private List<Integer> path2 = new ArrayList<>(); 
   
-    // Finds the path from root node to given root of the tree. 
     int findLCA(int n1, int n2) { 
         path1.clear(); 
         path2.clear();
@@ -43,17 +42,13 @@ public class LCA {
         return path1.get(i-1); 
     } 
       
-    // Finds the path from root node to given root of the tree, Stores the 
-    // path in a vector path[], returns true if path exists otherwise false 
     private boolean findPath(Node root, int n, List<Integer> path) 
     { 
-        // base case 
+   
         if (root == null) { 
             return false; 
         } 
           
-        // Store this node . The node will be removed if 
-        // not in path from root to n. 
         path.add(root.data); 
   
         if (root.data == n) { 
@@ -67,30 +62,11 @@ public class LCA {
         if (root.right != null && findPath(root.right, n, path)) { 
             return true; 
         } 
-  
-        // If not present in subtree rooted with root, remove root from 
-        // path[] and return false 
+
         path.remove(path.size()-1); 
   
         return false; 
     } 
 
 
-public static void main(String[] args) 
-{ 
-    LCA tree = new LCA(); 
-    tree.root = new Node(1); 
-    tree.root.left = new Node(2); 
-    tree.root.right = new Node(3); 
-    tree.root.left.left = new Node(4); 
-    tree.root.left.right = new Node(5); 
-    tree.root.right.left = new Node(6); 
-    tree.root.right.right = new Node(7); 
-
-    System.out.println("LCA(4, 5): " + tree.findLCA(4,5)); 
-    System.out.println("LCA(4, 6): " + tree.findLCA(4,6)); 
-    System.out.println("LCA(3, 4): " + tree.findLCA(3,4)); 
-    System.out.println("LCA(2, 4): " + tree.findLCA(2,4)); 
-  
-    }
 }
---
+++ Java/Test.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void testEmpty() {
+		LCA tree = new LCA();
+		assertEquals("Empty tree", -1, tree.findLCA(1,2)); 
+		
+	}
+	@org.junit.Test
+	public void testSingleNode() {
+		LCA tree = new LCA();
+		tree.root = new Node(1);
+		assertEquals("One Node tree", -1, tree.findLCA(1,2)); 
+	}
+	
+	@org.junit.Test
+	public void testThreeNodes() {
+		LCA tree = new LCA();
+		tree.root = new Node(1);
+	    tree.root.left = new Node(2); 
+	    tree.root.right = new Node(3); 
+		assertEquals("Three node tree", 1, tree.findLCA(2,3)); 
+	}
+	
+	@org.junit.Test
+	public void testBiggerTree() {
+	    LCA tree = new LCA(); 
+	    tree.root = new Node(1); 
+	    tree.root.left = new Node(2); 
+	    tree.root.right = new Node(3); 
+	    tree.root.left.left = new Node(4); 
+	    tree.root.left.right = new Node(5); 
+	    tree.root.right.left = new Node(6); 
+	    tree.root.right.right = new Node(7); 
+	    
+	   assertEquals("LCA(4, 5): ", 2, tree.findLCA(4,5)); 
+	   assertEquals("LCA(4, 6): ", 1, tree.findLCA(4,6)); 
+	   assertEquals("LCA(3, 4): ", 1, tree.findLCA(3,4)); 
+	   assertEquals("LCA(2, 4): ", 2, tree.findLCA(2,4)); 
+	   assertEquals("LCA(4, 7): ", 1, tree.findLCA(4,7)); 
+	}
+
+
+}
